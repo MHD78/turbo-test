35f391cdde8df8af2b67cbda4a249e21a77278f4
Mohamad Damirchi <79166420+MHD78@users.noreply.github.com>
2025-09-12 22:49:48 +0330

    Feat/try cd (#5)
    * feat/try-cd : implemented cd
    
    * feat/try-cd : fixed cd workflow
    
    * fix: add pnpm setup to CD workflow
    
    * fix: add basePath and assetPrefix for GitHub Pages deployment
    
    ---------
    
    Co-authored-by: Mohamad <m.damirchi@dataak.com>

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/turbo-test' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/turbo-test' : '',
   images: {
     unoptimized: true,
   },
